8ad20372f15dba3e4a6e00afd4ee5daff024178d
Melissa <melibene999@gmail.com>
2023-10-12 11:47:50 -0400

    styles and navbar

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -2,10 +2,13 @@ import 'navBar.css';
 
 function NavBar() {
   return (
-    <div className="NavBar">
-      <h1>Test</h1>
-    </div>
+    <nav className="navbar navbar-dark">
+      <a className="navbar-brand headingFont" href="#">
+        <img src="https://styles.redditmedia.com/t5_2sk2o/styles/communityIcon_aqe0q87y5pd61.png" width="30" height="30" className="d-inline-block align-top mx-2" alt="icon" />
+        ReadMe
+      </a>
+    </nav>
   );
 }
 
-export default App;
+export default NavBar;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app/App';
+import NavBar from './components/navBar/navBar';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery/dist/jquery.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <NavBar />
   </React.StrictMode>
 );
